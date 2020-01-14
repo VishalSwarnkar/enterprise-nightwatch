034ceb596f47f2373ef41a705e9ac307a6c8adc3
Vishal Swarnkar <vishal.swarnkar@tmp.com>
2020-01-14 20:22:34 +0530

    update the test scenario

--- src/tests/pages/home.page.js
+++ src/tests/pages/home.page.js
@@ -17,13 +17,34 @@ module.exports = {
         browser
             .setValue('input[name=email]', 'mailosaur.test@gmail.com')
             .setValue('input[name=password]', 'Automation!23')
-            .pause(60000)
             .waitForElementVisible('button[type=submit]', 1000)
             .click('button[type=submit]')
             .pause(1000)
             .assert.visible('input[placeholder=Search Projects]')
             .end()
     },
+
+    'Verify the user can add new project': browser => {
+        browser
+        .waitForElementVisible('.tw-project-box-content__new-project', 1000)
+        .click('.tw-project-box-content__new-project')
+        .pause(1000)
+        .waitForElementVisible('input[name=project-name]', 1000)
+        .assert.visible('div[data-user-email=mailosaur.test@gmail.com]')
+        .setValue('input[name=project-name]', 'Sample project01')
+        .waitForElementPresent('.tw-new-project__choose-workflow-button')
+        
+        browser.getText('.tw-new-project__choose-workflow-button', (result)=>{
+            this.assert.equal(result.value, 'Next: Choose a template')
+        })
+        browser.click()
+        .waitForElementVisible('.tw-modal-content__title')
+        browser.getText('.tw-modal-content__title', (result)=>{
+            this.assert.equal(result.value, 'Choose a Project Template')
+        })
+        browser.click()
+        .pause(2000)
+    }
 
     // afterAll: browser => {
     //     browser.end();
